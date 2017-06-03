6bc7815e04cde4cc37f7614bd1a0be6ab8d82689
Markus Wind <markuswind@gmail.com>
2017-06-03 15:56:29 +0200

    👷  - updated development script

--- .update-example-module.sh
+++ .update-example-module.sh
@@ -1,19 +1,13 @@
-echo "🗑  - removing old copy of react-native-select-input-ios .."
-rm -rf ./example/node_modules/react-native-select-input-ios
-
-echo "👷  - creating new copy of react-native-select-input-ios.."
-mkdir ./example/node_modules/react-native-select-input-ios
+echo "🗑  - removing old copy of source files .."
+rm -rf ./example/node_modules/react-native-select-input-ios/src
 
 for data in *; do
   if [ -d "$data" ]; then
-    if [ $data != "node_modules" ] && [ $data != "example" ] && [ $data != "screenshots" ]; then
+    if [ $data == "src" ]; then
       cp -R ./$data ./example/node_modules/react-native-select-input-ios/
-      echo "copied $data folder .."
+      echo "🚚  - copied $data folder .."
     fi
-  else
-    cp ./$data ./example/node_modules/react-native-select-input-ios/
-    echo "copied $data file .."
   fi
 done
 
-echo "🍻  - done!"
+echo "🔧  - done!"
