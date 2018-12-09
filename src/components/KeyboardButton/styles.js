40cb12f0e28d88bcfe5cd06f393bf18ba2eef196
Markus Wind <markuswind@gmail.com>
2018-12-09 13:22:26 +0100

    fix unnecessary line breaking on keyboard button

--- src/components/KeyboardButton/styles.js
+++ src/components/KeyboardButton/styles.js
@@ -1,10 +1,7 @@
 import { Dimensions, StyleSheet } from 'react-native'
 
-const BUTTON_WIDTH = Dimensions.get('window').width * 0.25
-
 const styles = StyleSheet.create({
   buttontext: {
-    width: BUTTON_WIDTH,
     color: '#006BFF',
     padding: 8
   }
